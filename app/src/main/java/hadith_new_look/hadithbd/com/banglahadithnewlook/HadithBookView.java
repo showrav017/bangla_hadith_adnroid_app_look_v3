c96da8ea6595ef66ce37334f180e5f0d48de8b5d
SAM Solutions Laptop <showrav017@gmail.com>
2016-12-06 18:14:08 +0600

    First Commit

---
+++ app/src/main/java/hadith_new_look/hadithbd/com/banglahadithnewlook/HadithBookView.java
@@ -0,0 +1,14 @@
+package hadith_new_look.hadithbd.com.banglahadithnewlook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HadithBookView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+}
