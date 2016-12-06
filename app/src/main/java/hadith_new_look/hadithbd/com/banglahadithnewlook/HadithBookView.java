e7a04fabcf550acf2c8c8fb312113b6bdeebde10
SAM Solutions Laptop <showrav017@gmail.com>
2016-12-06 18:57:04 +0600

    First Commit

--- app/src/main/java/hadith_new_look/hadithbd/com/banglahadithnewlook/HadithBookView.java
+++ app/src/main/java/hadith_new_look/hadithbd/com/banglahadithnewlook/HadithBookView.java
@@ -2,13 +2,24 @@ package hadith_new_look.hadithbd.com.banglahadithnewlook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class HadithBookView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.hadith_book_view);
 
-        
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.top_icons_hadith_book_view, menu);
+        return true;
     }
 }
